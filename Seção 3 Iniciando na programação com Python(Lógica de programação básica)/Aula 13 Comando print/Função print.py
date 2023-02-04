484a5255a547bb313f620408fe021029c90e61d7
Ariel Lisboa Lopes <101740188+ArielLopes888@users.noreply.github.com>
2023-02-04 11:38:51 -0300

    Add files via upload

---
+++ Seção 3 Iniciando na programação com Python(Lógica de programação básica)/Aula 13 Comando print/Função print.py
@@ -0,0 +1,12 @@
+print('Ariel', 'outra coisa')
+print('Ariel', 'Arielle', sep='-')
+print('Ariel', 'Arielle', sep='@', end='$')
+print('lindos')
+print('824', '176', '070', sep='.', end='-18')  # como eu fiz
+# não posso repetir argumentos na mesma linha
+
+print('824', '176', '070', sep='.', end='-')
+print('18')  # como o professor fez
+print('testando')
+print('824.176.070-18')  # mais fácil kkk
+# sep e end são argumentos da função print
---
+++ Seção 3 Iniciando na programação com Python(Lógica de programação básica)/aula 12 (comentários)/Aula 12 comentários.py
@@ -0,0 +1,13 @@
+# comentario
+print('linha 1')
+print('linha 2')
+# Falando sobre do que se trata essa linha
+# Outra linha
+print('linha 3')
+print('linha 4')  # Isso e um comentario
+print('linha 5')
+print('linha 6')  # mais um comentario
+"""print('linha 7')
+print('linha 8')  triplas aspas duplas comentario
+print('linha 9')"""
+# final
